ec8ffd9d16446e204d4e24ce250d26aae26c7d4b
Jie Chen <jiechenmc@gmail.com>
2022-11-08 18:14:48 -0500

    stacked

---
+++ main.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+year = 2022
+workbook = f"ARWU subject {year}-all subjects.xlsx"
+
+all_dfs = pd.read_excel(workbook, sheet_name=None)
+sheets = list(all_dfs.keys())
+
+master = pd.DataFrame()
+
+for i in range(len(sheets)):
+    curr_key = sheets[i]
+    curr = all_dfs[curr_key]
+
+    curr["Discipline"] = curr_key
+    curr["Year"] = year
+
+    master = pd.concat([master, curr])
+
+master.to_excel(f"{year}-stacked.xlsx", index=False)
